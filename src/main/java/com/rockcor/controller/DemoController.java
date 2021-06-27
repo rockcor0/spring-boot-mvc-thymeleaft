5ebb7d7e2bc7d211b6204c2569e1ca37cf31e23d
Ricardo Delgado <ridel007@gmail.com>
2021-06-26 21:26:57 -0500

    First

---
+++ src/main/java/com/rockcor/controller/DemoController.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.rockcor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author ricardodelgadocarreno
+ *
+ */
+@Controller
+public class DemoController {
+
+	@GetMapping("/hello")
+	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		
+		model.addAttribute("name", name);
+
+		return "greeting";
+	}
+
+}
